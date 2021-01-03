8b88abc63add5170bbbf3fd29aaf8653fc7e66ba
nikita-skobov <nskobov.websites@gmail.com>
2021-01-03 16:28:28 -0600

    implements rotate_texture about center and generic transforms

--- portion-renderer/src/transform.rs
+++ portion-renderer/src/transform.rs
@@ -1,7 +1,68 @@
+use std::f64::consts::PI;
+
 use super::RgbaPixel;
 use super::get_red_index;
 use super::Projection;
-use super::PIXEL_BLACK;
+
+macro_rules! rotate_point {
+    ($x:expr, $y:expr, $sin:expr, $cos:expr) => {
+        (($x * $cos) - ($y * $sin), ($x * $sin) + ($y * $cos))
+    };
+}
+
+pub fn min_f64(a: f64, b: f64, c: f64) -> f64 {
+    let c = if c < 0.0 { c } else { 0.0 };
+    let b = if b < c { b } else { c };
+    let a = if a < b { a } else { b };
+    a
+}
+
+pub fn max_f64(a: f64, b: f64, c: f64) -> f64 {
+    let c = if c > 0.0 { c } else { 0.0 };
+    let b = if b > c { b } else { c };
+    let a = if a > b { a } else { b };
+    a
+}
+
+pub fn rotated_size(width: usize, height: usize, angle: f64) -> (u32, u32) {
+    if width <= 0 || height <= 0 {
+        return (0, 0);
+    }
+
+    let width_f64 = width as f64;
+    let height_f64 = height as f64;
+
+    let radians  = PI * angle / 180.0;
+    let (sin, cos) = (radians.sin(), radians.cos());
+
+    let (x1, y1) = rotate_point!(width_f64 - 1.0, 0.0, sin, cos);
+    let (x2, y2) = rotate_point!(width_f64 - 1.0, height_f64 - 1.0, sin, cos);
+    let (x3, y3) = rotate_point!(0.0, height_f64 - 1.0, sin, cos);
+
+    let min_x = min_f64(x1, x2, x3);
+    let max_x = max_f64(x1, x2, x3);
+    let min_y = min_f64(y1, y2, y3);
+    let max_y = max_f64(y1, y2, y3);
+
+
+    let new_width = max_x - min_x + 1.0;
+    let new_width_floor = new_width.floor();
+    let new_width = if new_width - new_width_floor > 0.1 {
+        new_width_floor as u32 + 1
+    } else {
+        new_width_floor as u32
+    };
+
+    let new_height = max_y - min_y + 1.0;
+    let new_height_floor = new_height.floor();
+    let new_height = if new_height - new_height_floor > 0.1 {
+        new_height_floor as u32 + 1
+    } else {
+        new_height_floor as u32
+    };
+
+    (new_width, new_height)
+}
 
 
 fn blend_bilinear(
@@ -86,8 +147,64 @@ fn interpolate_bilinear(
     blend_bilinear(top_left, top_right, bottom_left, bottom_right, right_weight, bottom_weight)
 }
 
+pub fn rotate_texture_about_center(
+    texture: &[u8],
+    texture_width: u32,
+    texture_height: u32,
+    angle: f32,
+    default_pixel: RgbaPixel,
+) -> (Vec<u8>, u32, u32) {
+    let angle = angle - (angle / 360.0).floor() * 360.0;
+    let (dest_width, dest_height) = rotated_size(
+        texture_width as usize, texture_height as usize, angle as f64
+    );
+    let num_pixels = (dest_height * dest_width) as usize;
+    let indices_per_pixel = 4;
+    let mut dest = vec![0; indices_per_pixel * num_pixels];
+
+    let rotate = Projection::rotate(angle.to_radians());
+    let (cx, cy) = (texture_width as f32 / 2.0, texture_height as f32 / 2.0);
+    let rotate_about_center = Projection::translate(cx, cy) * rotate * Projection::translate(-cx, -cy);
+
+    transform_texture(
+        texture, texture_width, texture_height,
+        &rotate_about_center, default_pixel,
+        &mut dest, dest_width
+    );
+
+    (dest, dest_width, dest_height)
+}
+
+
+pub fn transform_texture(
+    texture: &[u8],
+    texture_width: u32,
+    texture_height: u32,
+    projection: &Projection,
+    default_pixel: RgbaPixel,
+    out_texture: &mut Vec<u8>,
+    out_width: u32,
+) {
+    let projection = projection.invert();
+
+    let indices_per_pixel = 4;
+    let pitch = indices_per_pixel * out_width as usize;
+    let chunks = out_texture.chunks_mut(pitch);
+
+    chunks.enumerate().for_each(|(y, row)| {
+        for (x, slice) in row.chunks_mut(indices_per_pixel).enumerate() {
+            let (px, py) = projection.map_affine(x as f32, y as f32);
+            let pixel = interpolate_bilinear(texture, texture_width, texture_height, px, py, default_pixel);
+            slice[0] = pixel.r;
+            slice[1] = pixel.g;
+            slice[2] = pixel.b;
+        }
+    });
+}
+
+
 #[cfg(test)]
-mod tests {
+mod transform_tests {
     use super::*;
 
     #[test]
@@ -131,5 +248,23 @@ mod tests {
             r: 24, b: 24, g: 24, a: 0,
         };
         assert_eq!(blended, expected_blended);
+    }
+
+    #[test]
+    fn rotated_size_works() {
+        // a 3x3 square:
+        // [ a a a
+        //   a a a
+        //   a a a
+        // ]
+        // rotated 45 degrees should need to be in a 4x4:
+        // [ x x a a
+        //   x a a a
+        //   a a a x
+        //   x x x x
+        //] ,,, something like that... its hard to do in ascii
+        let (new_width, new_height) = rotated_size(3, 3, 45.0);
+        assert_eq!(new_width, 4);
+        assert_eq!(new_height, 4);
     }
 }
