6f4eb8389d9f948662487360bb91f3fece6b2ecf
nikita-skobov <nskobov.websites@gmail.com>
2021-01-04 00:20:56 -0600

    adds tests for projection mod, adds matrix enum and tests

--- portion-renderer/src/projection.rs
+++ portion-renderer/src/projection.rs
@@ -137,7 +137,7 @@ enum TransformationClass {
 /// See ./examples/projection.rs for more examples.
 #[derive(Copy, Clone, Debug)]
 pub struct Projection {
-    transform: [f32; 9],
+    pub transform: [f32; 9],
     inverse: [f32; 9],
     class: TransformationClass,
 }
@@ -299,5 +299,306 @@ impl<'a, 'b> Mul<&'b (f32, f32)> for &'a Projection {
 
     fn mul(self, rhs: &(f32, f32)) -> (f32, f32) {
         *self * *rhs
+    }
+}
+
+
+/// given theta in radians, convert to a sin, cos
+/// rotation matrix
+pub fn get_rotation_matrix_radians(theta: f32) -> Matrix {
+    let (sin, cos) = theta.sin_cos();
+    Matrix::Rotate(cos, sin)
+}
+
+/// given an angle in degrees, convert to a sin, cos
+/// rotation matrix, this is just a convenience
+/// function for calling get_rotation_matrix_radians
+pub fn get_rotation_matrix(angle: f32) -> Matrix {
+    let radians = angle.to_radians();
+    get_rotation_matrix_radians(radians)
+}
+
+pub enum Matrix {
+    Unit,
+    Scale(f32, f32),
+    TranslateXY(f32, f32),
+    /// cos, sin
+    Rotate(f32, f32),
+    /// scalex, scaley, translatex, translatey
+    ScaleAndTranslate(f32, f32, f32, f32),
+    /// cos, sin, translatex, translatey
+    RotateAndTranslate(f32, f32, f32, f32),
+    /// 0, 1, 3, 4, translatex, translatey
+    RotateAndScaleAndTranslate(f32, f32, f32, f32, f32, f32),
+}
+
+impl Mul<(f32, f32)> for Matrix {
+    type Output = (f32, f32);
+
+    #[inline(always)]
+    fn mul(self, rhs: (f32, f32)) -> Self::Output {
+        match self {
+            Matrix::Unit => rhs,
+            Matrix::Scale(sx, sy) => (sx * rhs.0, sy * rhs.1),
+            Matrix::Rotate(cos, sin) => (cos * rhs.0 - sin * rhs.1, sin * rhs.0 + cos * rhs.1),
+            Matrix::TranslateXY(by_x, by_y) => (rhs.0 + by_x, rhs.1 + by_y),
+            Matrix::ScaleAndTranslate(sx, sy, by_x, by_y) => (sx * rhs.0 + by_x, sy * rhs.1 + by_y),
+            Matrix::RotateAndTranslate(cos, sin, by_x, by_y) => (cos * rhs.0 - sin * rhs.1 + by_x, sin * rhs.0 + cos * rhs.1 + by_y),
+            Matrix::RotateAndScaleAndTranslate(a0, a1, b0, b1, by_x, by_y) => (a0 * rhs.0 + a1 * rhs.1 + by_x, b0 * rhs.0 + b1 * rhs.1 + by_y),
+        }
+    }
+}
+
+impl From<&Matrix> for [f32; 9] {
+    fn from(m: &Matrix) -> Self {
+        match m {
+            Matrix::Unit => [
+                1.0, 0.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 0.0, 1.0],
+            Matrix::Scale(sx, sy) => [
+                *sx, 0.0, 0.0,
+                0.0, *sy, 0.0,
+                0.0, 0.0, 1.0],
+            Matrix::Rotate(cos, sin) => [
+                *cos, -*sin, 0.0,
+                *sin, *cos, 0.0,
+                0.0, 0.0, 1.0],
+            Matrix::TranslateXY(x, y) => [
+                1.0, 0.0, *x,
+                0.0, 1.0, *y,
+                0.0, 0.0, 1.0],
+            Matrix::ScaleAndTranslate(sx, sy, by_x, by_y) => [
+                *sx, 0.0, *by_x,
+                0.0, *sy, *by_y,
+                0.0, 0.0, 1.0],
+            Matrix::RotateAndTranslate(cos, sin, by_x, by_y) => [
+                *cos, -*sin, *by_x,
+                *sin, *cos, *by_y,
+                0.0, 0.0, 1.0],
+            Matrix::RotateAndScaleAndTranslate(a0, a1, b0, b1, by_x, by_y) => [
+                *a0, *a1, *by_x,
+                *b0, *b1, *by_y,
+                0.0, 0.0, 1.0],
+        }
+    }
+}
+
+impl From<Matrix> for [f32; 9] {
+    fn from(m: Matrix) -> Self {
+        (&m).into()
+    }
+}
+
+pub fn print_matrix(m: &Matrix) {
+    let matrix: [f32; 9] = m.into();
+    print_matrix3(matrix);
+}
+pub fn print_matrix3(matrix: [f32; 9]) {
+    for row in matrix.chunks(3) {
+        for j in row {
+            print!("{}, ", j);
+        }
+        println!("");
+    }
+}
+
+impl Mul<Matrix> for Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: Matrix) -> Self::Output {
+        let matrix_self: [f32; 9] = self.into();
+        let matrix_rhs: [f32; 9] = rhs.into();
+
+        let multiplied = mul3x3(matrix_self, matrix_rhs);
+
+        let has_scale = match (multiplied[0], multiplied[4]) {
+            (x, y) => if x == 1.0 && y == 1.0 {
+                None
+            } else {
+                Some((x, y))
+            }
+        };
+        let has_translate = match (multiplied[2], multiplied[5]) {
+            (x, y) => if x == 0.0 && y == 0.0 {
+                None
+            } else {
+                Some((x, y))
+            }
+        };
+        let has_rotate = match (multiplied[1], multiplied[3]) {
+            (x, y) => if x == 0.0 && y == 0.0 {
+                None
+            } else {
+                Some((multiplied[0], multiplied[1], multiplied[3], multiplied[4]))
+            }
+        };
+
+        let is_sin_and_cos = multiplied[0] == multiplied[4] && multiplied[1] == -multiplied[3];
+        // check if its just rotate:
+        if is_sin_and_cos && has_translate.is_none() {
+            return Matrix::Rotate(multiplied[0], multiplied[3]);
+        } else if is_sin_and_cos && has_translate.is_some() {
+            let (tx, ty) = has_translate.unwrap(); // safe because is_some
+            return Matrix::RotateAndTranslate(multiplied[0], multiplied[3], tx, ty);
+        };
+
+        match (has_scale, has_translate, has_rotate) {
+            (None, None, None) => Matrix::Unit,
+            (None, Some(translate), None) => Matrix::TranslateXY(translate.0, translate.1),
+            (Some(scale), None, None) => Matrix::Scale(scale.0, scale.1),
+            (Some(scale), Some(translate), None) => Matrix::ScaleAndTranslate(scale.0, scale.1, translate.0, translate.1),
+
+            // I dont see a point in handling scale/rotate combinations
+            // because their matrix positions overlap
+            (_, None, Some(r)) => Matrix::RotateAndScaleAndTranslate(r.0, r.1, r.2, r.3, 0.0, 0.0),
+            (_, Some(t), Some(r)) => Matrix::RotateAndScaleAndTranslate(r.0, r.1, r.2, r.3, t.0, t.1),
+        }
+    }
+}
+
+#[cfg(test)]
+mod projection_tests {
+    use super::*;
+
+    #[inline(always)]
+    fn assert_f_eq(float_left: f32, float_right: f32) {
+        let threshhold = 10000.0;
+        let left = f32::trunc(float_left * threshhold) / threshhold;
+        let right = f32::trunc(float_right * threshhold) / threshhold;
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn basic_matrix_multiplication_works() {
+        let m_scale = Matrix::Scale(2.0, 3.0);
+        let m_trans = Matrix::TranslateXY(1.0, 1.5);
+        // matrix multiplication 'goes backwards'
+        // this will first scale, then it will translate
+        let m = m_trans * m_scale;
+        if let Matrix::ScaleAndTranslate(_, _, _, _) = m {
+            assert!(true);
+        } else {
+            print_matrix(&m);
+            panic!("Matrix is not a scale and translate matrix");
+        }
+        let matrix: [f32; 9] = m.into();
+        assert_eq!(matrix,
+        [
+            2.0, 0.0, 1.0,
+            0.0, 3.0, 1.5,
+            0.0, 0.0, 1.0,
+        ]);
+
+        // lets do the above in reverse order:
+        let m_scale = Matrix::Scale(2.0, 3.0);
+        let m_trans = Matrix::TranslateXY(1.0, 1.5);
+        let m = m_scale * m_trans;
+        let matrix: [f32; 9] = m.into();
+        assert_eq!(matrix,
+        [
+            2.0, 0.0, 2.0,
+            0.0, 3.0, 4.5,
+            0.0, 0.0, 1.0,
+        ]);
+    }
+
+    #[test]
+    fn simple_rotate_works() {
+        let (x, y) = (1.0, 0.0);
+        let p = Projection::rotate(90f32.to_radians());
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 0.0);
+        assert_f_eq(out_y, 1.0);
+
+        let p = Projection::rotate((-90f32).to_radians());
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 0.0);
+        assert_f_eq(out_y, -1.0);
+
+        let p = Projection::rotate(45f32.to_radians());
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 0.70712);
+        assert_f_eq(out_y, 0.70712);
+
+        // going over 360 loops back
+        // so p1 should have same result as p2
+        let p1 = Projection::rotate(361f32.to_radians());
+        let p2 = Projection::rotate(1f32.to_radians());
+        let (p1_x, p1_y) = p1.map_projective(x, y);
+        let (p2_x, p2_y) = p2.map_projective(x, y);
+        assert_f_eq(p1_x, p2_x);
+        assert_f_eq(p1_y, p2_y);
+    }
+
+    #[test]
+    fn can_scale() {
+        let (x, y) = (1.0, 0.0);
+        let p = Projection::scale(2.0, 1.0);
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 0.0);
+
+        let m = Matrix::Scale(2.0, 1.0);
+        let (out_x, out_y) = m * (x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 0.0);
+    }
+
+    #[test]
+    fn can_translate() {
+        let (x, y) = (1.0, 0.0);
+        let p = Projection::translate(1.0, 1.0);
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 1.0);
+
+        let m1 = Matrix::TranslateXY(1.0, 0.0);
+        let m2 = Matrix::TranslateXY(0.0, 1.0);
+        let m3 = m1 * m2;
+        let (out_x, out_y) = m3 * (x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 1.0);
+
+        let m = Matrix::TranslateXY(1.0, 1.0);
+        let (out_x, out_y) = m * (x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 1.0);
+    }
+
+    #[test]
+    fn can_rotate_and_scale() {
+        let (x, y) = (1.0, 0.0);
+        let p = Projection::rotate(90f32.to_radians()) * Projection::scale(2.0, 1.0);
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 0.0);
+        assert_f_eq(out_y, 2.0);
+
+        let rotation_matrix = get_rotation_matrix(90f32);
+        let scale_matrix = Matrix::Scale(2.0, 1.0);
+        let m = rotation_matrix * scale_matrix;
+        let (out_x, out_y) = m * (x, y);
+        assert_f_eq(out_x, 0.0);
+        assert_f_eq(out_y, 2.0);
+    }
+
+    #[test]
+    fn can_rotate_about_arbitrary_point() {
+        let (x, y) = (1.0, 0.0);
+        // normally a rotation 90d would move (1, 0) to (0, 1)
+        // but here we rotate about an arbitrary point: (1, 1)
+        // such that when our (1, 0) gets rotate 90d, it should become (2, 1)
+        let p = Projection::translate(1.0, 1.0) * Projection::rotate(90f32.to_radians()) * Projection::translate(-1.0, -1.0);
+        let (out_x, out_y) = p.map_projective(x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 1.0);
+
+        let r = get_rotation_matrix(90f32);
+        let t1 = Matrix::TranslateXY(1.0, 1.0);
+        let t2 = Matrix::TranslateXY(-1.0, -1.0);
+        let m = t1 * r * t2;
+        let (out_x, out_y) = m * (x, y);
+        assert_f_eq(out_x, 2.0);
+        assert_f_eq(out_y, 1.0);
     }
 }
